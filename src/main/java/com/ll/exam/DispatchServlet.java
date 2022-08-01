171a5f93874f831fce7a3c6891754a14611ee17f
Aruming <0430kar@naver.com>
2022-08-01 11:25:11 +0900

    기존의 room을 roomManual로 복사, room에 ajax 적용 준비

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -24,6 +24,10 @@ public class DispatchServlet extends HttpServlet {
         switch (rq.getRouteMethod()){
             case "GET":
                 switch (rq.getActionPath()) {
+                    case "/usr/chat/roomManual":
+                        chatController.showRoomManual(rq);
+                        break;
+
                     case "/usr/chat/room":
                         chatController.showRoom(rq);
                         break;
